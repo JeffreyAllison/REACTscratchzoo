7fa61e557af1424a34c2f961547b81d852f6ab81
JeffAllison <itsjallison@gmail.com>
2022-04-29 12:00:48 -0700

    added verbiage to app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/dragon/i);
+  const linkElement = screen.getByText(/animal/i);
   expect(linkElement).toBeInTheDocument();
 });
