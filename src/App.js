11647ccba7c1dfad7ea08b87eb26feda22fcb92d
JeffAllison <itsjallison@gmail.com>
2022-05-19 10:22:17 -0700

    moved app function into zoo.js page, imported zoo.js into app.js and formatted app.js page with react-router-dom to generate links between admin page and home zoo page

--- src/App.js
+++ src/App.js
@@ -1,106 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import ZooOpen from './ZooOpen.js';
 import Zoo from './Zoo.js';
-import AnimalParade from './AnimalParade.js';
-import Button from '@mui/material/Button';
-import CustomButton from './CustomButton';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import AdminPage from './AdminPage';
 
-function App() {
-  const [dragonPower, setDragonPower] = useState(3);
-  const [mammothPower, setMammothPower] = useState(3);
-  const ATTACK_MODIFIER = 0.8;
-  const [animal, setAnimal] = useState(['']);
-  const [isOpen, setIsOpen] = useState(false);
-
-  function handleDragonAttack() {
-    setMammothPower(mammothPower - ATTACK_MODIFIER);
-  }
-
-  function handleDragonScales() {
-    setDragonPower(dragonPower + ATTACK_MODIFIER);
-  }
-
-  function handleMammothAttack() {
-    setDragonPower(dragonPower - ATTACK_MODIFIER);
-  }
-
-  function handleMammothTusks() {
-    setMammothPower(mammothPower + ATTACK_MODIFIER);
-  }
-
-  function handleAddDodo() {
-    animal.push('dodo');
-    setAnimal([...animal]);
-  }
-
-  function handleAddGrey() {
-    animal.push('grey');
-    setAnimal([...animal]);
-  }
-
-  function handleAddGorilla() {
-    animal.push('gorilla');
-    setAnimal([...animal]);
-  }
-
-  function handleAddBat() {
-    animal.push('bat');
-    setAnimal([...animal]);
-  }
-
-  function handleAddDragon() {
-    animal.push('dragon');
-    setAnimal([...animal]);
-  }
-
-  function handleAddMammoth() {
-    animal.push('mammoth');
-    setAnimal([...animal]);
-  }
-
-  function handleOpenZoo() {
-    setIsOpen(true);
-  }
-
-  function handleCloseZoo() {
-    setIsOpen(false);
-  }
-
+export default function App() {
   return (
-    <div className="App">
-      <div className="fight">
-        <div className="fighter">
-          <div className="emoji" style={{ fontSize: `${dragonPower}rem` }}>
-            🐲
-          </div>
-          <CustomButton onClick={handleDragonAttack}>The Dragon attacks the Mammoth</CustomButton>
-          <CustomButton onClick={handleDragonScales}>
-            The Dragon regenerates its scales
-          </CustomButton>
-        </div>
-        <div className="fighter">
-          <div className="emoji" style={{ fontSize: `${mammothPower}rem` }}>
-            🦣
-          </div>
-          <CustomButton onClick={handleMammothAttack}>The Mammoth attacks the Dragon</CustomButton>
-          <CustomButton onClick={handleMammothTusks}>The Mammoth sharpens its tusks</CustomButton>
-        </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home Page</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin Page</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/admin">
+            <AdminPage />
+          </Route>
+          <Route path="/">
+            <Zoo />
+          </Route>
+        </Switch>
       </div>
-      <AnimalParade animals={animal} />
-      <div className="animal-buttons">
-        <CustomButton onClick={handleAddDodo}>Dodo</CustomButton>
-        <CustomButton onClick={handleAddGrey}>Grey</CustomButton>
-        <CustomButton onClick={handleAddGorilla}>Gorilla</CustomButton>
-        <CustomButton onClick={handleAddBat}>Bat</CustomButton>
-        <CustomButton onClick={handleAddDragon}>Dragon</CustomButton>
-        <CustomButton onClick={handleAddMammoth}>Mammoth</CustomButton>
-      </div>
-      <ZooOpen isOpen={isOpen} />
-      <CustomButton onClick={handleOpenZoo}>Enter</CustomButton>
-      <CustomButton onClick={handleCloseZoo}>Exit</CustomButton>
-    </div>
+    </Router>
   );
 }
-
-export default App;
