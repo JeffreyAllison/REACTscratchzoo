4b1092f1bac3634f01d254294971c8cde4d7cb4c
JeffAllison <itsjallison@gmail.com>
2022-05-19 15:08:32 -0700

    installed victory into app, imported VictoryBar and VictoryChart into barchart.js, called VictoryChart and Victory Bar into BarChart function, rendered bar chart data

--- src/BarChart.js
+++ src/BarChart.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import reactDom from 'react-dom';
+import { VictoryBar, VictoryChart } from 'victory';
 
 export default function BarChart() {
-  return <div>BarChart</div>;
+  return (
+    <div>
+      <VictoryChart domainPadding={30}>
+        <VictoryBar
+          width={300}
+          data={[
+            { quarter: 1, earnings: 800 },
+            { quarter: 2, earnings: 900 },
+            { quarter: 3, earnings: 100 },
+            { quarter: 4, earnings: 222 },
+          ]}
+          x="quarter"
+          y="earnings"
+        ></VictoryBar>
+      </VictoryChart>
+    </div>
+  );
 }
--- src/PieChart.js
+++ src/PieChart.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import reactDom from 'react-dom';
+import { VictoryPie, VictoryChart, VictoryTheme } from 'victory';
 
 export default function PieChart() {
   return <div>PieChart</div>;
